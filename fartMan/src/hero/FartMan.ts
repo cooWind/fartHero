8d5b7289f06017e396ed2b9d0c1aed591829eb8b
duhao <duhao@dxy.cn>
2018-10-30 10:11:27 +0800

    fea: hero class

--- fartMan/src/hero/FartMan.ts
+++ fartMan/src/hero/FartMan.ts
@@ -1,45 +1,54 @@
 /* 
     放屁超人
 **/
-class FartMan extends egret.Sprite{
-    private parents
-    public constructor(parent){
-        super()
-        this.addEventListener(egret.Event.ADDED_TO_STAGE,this.addMan,this);
-        this.parents = parent
+interface createBoxConfig {
+        width: number;
+        height: number;
+        position: Object;
+        type?: any;
+}
+class FartMan {
+    public constructor(){
+
     }
-  
-    private async addFlat(){
-        this.addMan()
-    }
-    private addMan() {
-        const factor = 50
-        const sh = GameConfig.height / 50
-        const sw = GameConfig.width / 50
-        const world = this.parents.world
-         //添加长方形刚体
-        var boxShape: p2.Shape = new p2.Box({
-            width: 1,
-            height: 1
+    /**
+     * 可以创建一个英雄对象，内置了白鹭的创建形状的方法,
+     * 你无需关注它是如何实现的，你只要将接口中的参数传给它就行
+     */
+    public drawMan(createBoxConfig: createBoxConfig) {
+        
+        const {
+            width,
+            height,
+            position
+        } = createBoxConfig
+        const boxShape: p2.Shape = new p2.Box({
+            width: width,
+            height: height
         });
-        var boxBody: p2.Body = new p2.Body({ mass: 1,position: [sw / 2,0],angularVelocity: 3 });
+        const boxBody: p2.Body = new p2.Body({ 
+            mass: 1,
+            position:position,
+            fixedRotation: true
+        });
         boxBody.addShape(boxShape);
-        world.addBody(boxBody);
         //添加长方形刚体的显示对象   
-        var display: egret.DisplayObject = this.createSprite();
-        display.width = (<p2.Box>boxShape).width * factor;
-        display.height = (<p2.Box>boxShape).height * factor;
-        display.anchorOffsetX = display.width / 2
-        display.anchorOffsetY = display.height / 2;
-        //同步egret对象和p2对象
+        var display: egret.DisplayObject = this.createSprite((<p2.Box>boxShape).width*50, (<p2.Box>boxShape).height*50);
+        console.log(display)
+        //同步egret对象和p2对象     
         boxBody.displays = [display];
-        this.addChild(display)
+        return {
+            boxBody: boxBody,
+            display
+        }
     }
-    private createSprite(): egret.Sprite {
+    private createSprite(width, height): egret.Sprite {
         var result: egret.Sprite = new egret.Sprite();
         result.graphics.beginFill(0x37827A);
-        result.graphics.drawRect(0,0,40,80);
+        result.graphics.drawRect(0,0,width,height);
         result.graphics.endFill();
+        result.anchorOffsetX = width / 2;
+        result.anchorOffsetY = height / 2;
         return result;
     }
 }
--- fartMan/src/router/flat.ts
+++ fartMan/src/router/flat.ts
@@ -1,14 +1,8 @@
 /// <reference path="../../libs/keydown/keydown_event.d.ts"/>
 /* 
     平台类
+    http://www.dwenzhao.cn/profession/netbuild/egretp2.html
 **/
-
-interface createBoxConfig {
-        width: number;
-        height: number;
-        position: Object;
-        type?: any;
-}
 class Flat extends gameMap{
     // 刚体世界　
     public world:p2.World
@@ -16,29 +10,21 @@ class Flat extends gameMap{
     private debugDraw
     private sh = GameConfig.height / 50
     private sw = GameConfig.height / 50
-    
+    private timeOnEnterFrame
+    private fartMan:FartMan
     public constructor(parent){
         super()
         this.addEventListener(egret.Event.ADDED_TO_STAGE,this.addFlat,this);
-        this.addEventListener(egret.Event.ENTER_FRAME, this.loop, this);
-    }
-    private createDebug(): void {
-        //创建调试试图
-        //this.debugDraw = new p2DebugDraw(this.world);
-        var sprite: egret.Sprite = new egret.Sprite();
-        this.addChild(sprite);
-        this.debugDraw.setSprite(sprite);
+        this.addEventListener(egret.Event.ENTER_FRAME, this.loop, this); 
+        this.timeOnEnterFrame = egret.getTimer();
     }
     private async addFlat(){
+        this.fartMan = new FartMan()
         await this.loadMap()
-        //this.addBack()
         this.createWorld()
-        this.createPlane()
-        // this.createMan()
-        this.createBar()
         this.controlKey()
         this.createText()
-        // this.createDebug()
+        this.bindP2Map()
     }
     // 创建世界
     private createWorld() {
@@ -48,127 +34,45 @@ class Flat extends gameMap{
         // this.world.sleepMode = p2.World.NO_SLEEPING;
         // 这玩意儿是求解器
         this.world.solver = new p2.GSSolver() 
-        this.world.solver['iterations'] = 50
-        this.world.solver['tolerance'] = 0.01
+        this.world.solver['iterations'] = 10
+        this.world.solver['tolerance'] = 0
         // 设置摩擦力
         this.world.defaultContactMaterial.friction = 10;
         // 设置刚度，很硬的那种
         this.world.setGlobalStiffness(99999);
-        this.world.setGlobalRelaxation(1.8)
-        this.world.defaultContactMaterial.restitution = 0;
-        // var iceMaterial = new p2.Material(2);
-        // var steelMaterial = new p2.Material(2);
-        // const factor = 50
-        // // 摩擦配置
-        // var iceSteelContactMaterial: p2.ContactMaterial = new p2.ContactMaterial(iceMaterial,steelMaterial,<p2.ContactMaterialOptions>{surfaceVelocity: 0,restitution:0,friction: .6});
-        // this.world.addContactMaterial(iceSteelContactMaterial);
-        // //添加帧事件侦听
-        // egret.Ticker.getInstance().register((dt) => {
-        //     //使世界时间向后运动
-        //     this.world.step(dt / 1000);
-        //     // 这里更新坐标
-        //     var l = this.world.bodies.length;
-        //     for (var i:number = 0; i < l; i++) {
-        //         var boxBody:p2.Body = this.world.bodies[i];
-        //         var box:egret.DisplayObject = boxBody.displays[0];
-        //         if (box) { 
-        //                 box.x = boxBody.position[0] * factor;
-        //             box.y = -boxBody.position[1] * factor;
-        //         }
-        //     }
-        //     console.log(this.boxX)
-        //     this.boxBody.position[0] += this.boxX
-        // },this);
-    }
-    // 画一张#333的背景图
-    private addBack() {
-        let shp:egret.Shape = new egret.Shape();
-        shp.graphics.beginFill( 0x333333, 1);
-        shp.graphics.drawRect( 0, 0, GameConfig.width, GameConfig.height );
-        shp.graphics.endFill();
-        this.addChild( shp );
+        this.world.setGlobalRelaxation(1.9)
+        this.world.defaultContactMaterial.restitution = 0.1;
     }
     private display;
     private boxBody:p2.Body;
     private boxX = 0
     private boxY = 0
     private pbody
-    // 创建地面
-    private createPlane() {
-        const factor = 50
-        //创建地面       
-        var gshape: p2.Plane = new p2.Plane();
-        var gbody: p2.Body = new p2.Body({
-                position:[GameConfig.width/100, 2],
-                mass: 0
-            });
-        gbody.addShape(gshape);
-        this.world.addBody(gbody)
-        //显示对象
-        var ground: egret.DisplayObject = this.createSprite(GameConfig.width, 10);
-        gbody.displays = [ground];
-        this.addChild(ground);
-    }
-    // 创建人物
-    private createMan() {
-        var boxShape: p2.Shape = new p2.Box({
-            width: 100, 
-            height: 50, 
-            material: new p2.Material(1)
-        });
-        var boxBody: p2.Body = new p2.Body({ 
-            mass: 1, 
-            position: [200, GameConfig.height -125],
-            type: p2.Body.DYNAMIC,
-            fixedRotation: true
-        });
-        boxBody.addShape(boxShape);
-        this.world.addBody(boxBody);
-        this.boxBody = boxBody
-    }
-    // 创建障碍物
-    private createBar() {
-         // 创建平台
-        var plat: p2.Box = new p2.Box({
-            width: 1,
-            height: 1
-        });
-        var pbody: p2.Body = new p2.Body({
-                position:[GameConfig.width/25,GameConfig.height/25 - 100/50],
-                mass: 5
-            });
-        pbody.addShape(plat);
-        this.world.addBody(pbody)
-        //显示对象
-        var ground2: egret.DisplayObject = this.createPlat();
-
-        ground2.width = (<p2.Box>plat).width * 59;
-        ground2.height = (<p2.Box>plat).height * 50;
-        ground2.anchorOffsetX = ground2.width / 2
-        ground2.anchorOffsetY = ground2.height / 2;
-        pbody.displays = [ground2];
-        this.addChild(ground2);
-        this.world.addBody(pbody)
-    }    
+    
     private createText() {
-        this.boxBody = this.createBox({
+        const {
+            boxBody,
+            display
+        } = this.fartMan.drawMan({
             width: 1,
             height: 1,
-            position: [100/ 50,100/ 50]
+            position: [100 / 50,150/ 50]
         })
-        this.bindP2Map()
-        // this.createBox({
-        //     width: 1,
-        //     height: 1,
-        //     position: [200/ 50,100/ 50],
-        //     type: p2.Body.STATIC
-        // })
+        this.addChild(display)
+        this.world.addBody(boxBody)
+        this.boxBody = boxBody
     }
-    private loop(): void {
+    private loop(event): void {
         const factor = 50;
+        const fixedTimeStep = 60 / 1000
+         let now = egret.getTimer();
+        let pass = now - this.timeOnEnterFrame;
+        let dt:number = 1000 / pass;
+        egret.log("onEnterFrame: ", dt);
+        this.timeOnEnterFrame = egret.getTimer();
         if(!this.world || !this.boxBody)
             return;
-        this.world.step(60 / 1000);
+        this.world.step(1/80, dt/1000, 10);
         //this.debugDraw.drawDebug();
         this.boxBody.position[0] += this.boxX
         // this.boxBody.velocity[0] = this.boxX
@@ -193,11 +97,11 @@ class Flat extends gameMap{
         }
         keydown_event(37,()=>{
             console.log(37)
-                this.boxX = -.3; 
+                this.boxX = -.1; 
         },upEvent,upSelfEvent)
         keydown_event(39,()=>{
             console.log(39)
-                this.boxX = .3;
+                this.boxX = .1;
         },upEvent,upSelfEvent)
         keydown_event(38,()=>{
             console.log(38)
@@ -206,7 +110,7 @@ class Flat extends gameMap{
             console.log(40)
         },upEvent,upSelfEvent);
         keydown_event(67,()=>{  //c 
-            this.boxBody.velocity[1] = 5;
+            this.boxBody.velocity[1] = 6;
             //this.boxBody.position[1]+= .5
         });
         // keydown_event(88,()=>{  // x
@@ -281,53 +185,6 @@ class Flat extends gameMap{
         boxBody.addShape(boxShape);
         this.world.addBody(boxBody);
         boxBody.displays = [display];
-    }
-    /**
-     * 可以创建一个刚体对象，内置了白鹭的创建形状的方法,
-     * 你无需关注它是如何实现的，你只要将接口中的参数传给它就行
-     */
-    private createBox(createBoxConfig: createBoxConfig): p2.Body {
-        // const text:tiled.TMXLayer = this.tmxtileMap.getLayers()[4]
-        // const layers = this.tmxtileMap.getLayers()
-        // var hero;
-        // for(let i = 0, len = layers.length; i < len; i++) {
-        //     if(layers[i].name === 'hero') {
-        //         hero = layers[i]
-        //     }
-        // }
-        // const display:tiled.TMXLayer = hero.$children[0]
-        // display.anchorOffsetX = display.width / 2
-        // display.anchorOffsetY = display.height / 2
-        const {
-            width,
-            height,
-            position
-        } = createBoxConfig
-        // const position = [
-        //     display.x / 50,
-        //     display.y / 50
-        // ]
-        // const factor = 50
-        const boxShape: p2.Shape = new p2.Box({
-            width: width,
-            height: height
-        });
-        const boxBody: p2.Body = new p2.Body({ 
-            mass: 1,
-            position:position,
-            fixedRotation: true,
-            collisionResponse: true
-        });
-        boxBody.addShape(boxShape);
-        this.world.addBody(boxBody);
-        //添加长方形刚体的显示对象   
-        
-        var display: egret.DisplayObject = this.createSprite((<p2.Box>boxShape).width*50, (<p2.Box>boxShape).height*50);
-        this.addChild(display)
-        console.log(display)
-        //同步egret对象和p2对象     
-        boxBody.displays = [display];
-        return boxBody
     }
     private createSprite(width, height): egret.Sprite {
         var result: egret.Sprite = new egret.Sprite();
